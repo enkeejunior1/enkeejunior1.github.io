23c7ec7728c863b9dec22de8115ab452854594a4
enkeejunior1 <67228941+enkeejunior1@users.noreply.github.com>
2025-06-02 06:08:24 +0000

    Deploying to gh-pages from @ enkeejunior1/enkeejunior1.github.io@e09b4bf3c3166c99ccf63239bebbbc915f2feb09 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,63 +51,13 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-will-be-joining-upenn-cis-as-a-ph-d-student-starting-fall-2025-advised-by-prof-jiatao-gu",
-          title: 'I will be joining UPenn CIS as a Ph.D. student starting Fall 2025,...',
-          description: "",
-          section: "News",},{id: "news-our-paper-jump-your-steps-optimizing-sampling-schedule-of-discrete-diffusion-models-has-been-accepted-at-iclr-2025-mentor-chieh-hsin-jesse-lai",
+            },{id: "news-our-paper-jump-your-steps-optimizing-sampling-schedule-of-discrete-diffusion-models-has-been-accepted-at-iclr-2025-mentor-chieh-hsin-jesse-lai",
           title: 'Our paper “Jump Your Steps: Optimizing Sampling Schedule of Discrete Diffusion Models” has...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{id: "news-i-will-be-joining-upenn-cis-as-a-ph-d-student-starting-fall-2025-advised-by-prof-jiatao-gu",
+          title: 'I will be joining UPenn CIS as a Ph.D. student starting Fall 2025,...',
+          description: "",
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
